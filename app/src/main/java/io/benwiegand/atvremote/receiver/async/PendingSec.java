91444cbffa1f632f35498aba87221d964dec902b
Benjamin Wiegand <126627496+Benjamin-Wiegand@users.noreply.github.com>
2025-07-24 20:56:36 -0700

    allow flat-mapping PendingSec and Sec within PendingSec

--- app/src/main/java/io/benwiegand/atvremote/receiver/async/PendingSec.java
+++ app/src/main/java/io/benwiegand/atvremote/receiver/async/PendingSec.java
@@ -1,11 +1,13 @@
 package io.benwiegand.atvremote.receiver.async;
 
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class PendingSec<T> {
     private final Object lock = new Object();
 
     private boolean started = false;
+    private boolean wrapped = false;
 
     private final Supplier<Sec<T>> secStarter;
 
@@ -20,4 +22,22 @@ public class PendingSec<T> {
         }
         return secStarter.get();
     }
+
+    private <U> PendingSec<U> wrapSec(Function<Sec<T>, Sec<U>> mapping) {
+        synchronized (lock) {
+            if (started) throw new IllegalStateException("can't wrap after call to start()");
+            if (wrapped) throw new IllegalStateException("already wrapped!");
+            wrapped = true;
+            return new PendingSec<>(() -> mapping.apply(this.start()));
+        }
+    }
+
+    public <U> PendingSec<U> flatMapSec(Function<T, Sec<U>> mapping) {
+        return wrapSec(sec -> sec.flatMap(mapping));
+    }
+
+    public <U> PendingSec<U> flatMap(Function<T, PendingSec<U>> mapping) {
+        return flatMapSec(r -> mapping.apply(r).start());
+    }
+
 }
